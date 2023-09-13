14c9b6203d414d1f064ede3e54bed4b1f7963469
YeEun <ydkrkek@gmail.com>
2023-09-13 15:21:50 +0900

    [Update] Try2 낮은 높이에서 점프 부스터 & Try1 작업 내용 삭제

--- MiniGame/Assets/Scripts/Player.cs
+++ MiniGame/Assets/Scripts/Player.cs
@@ -1,34 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    public float jumpPower = 5;
-    TextMesh scoreOutput;
-    int score = 0;
+	public float jumpPower = 5;
+	public float lowWarn = -4;
+	public float jumpBoost = 2.5f;
 
-    void Start()
-    {
-        // 이름으로 게임 오브젝트를 찾고, 그 중 TextMesh 컴포넌트를 얻기
-        scoreOutput = GameObject.Find(name: "Score").GetComponent<TextMesh>();
-    }
+	TextMesh scoreOutput;
+	int score = 0;
 
-	void Update()
-    {
-        if (Input.GetButtonDown("Jump"))
-            GetComponent<Rigidbody>().velocity = new Vector3(0, jumpPower, 0);
-    }
-	private void OnCollisionEnter(Collision collision)
+	void Start()
 	{
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		// 이름으로 게임 오브젝트를 찾고, 그 중 TextMesh 컴포넌트를 얻기
+		scoreOutput = GameObject.Find(name: "Score").GetComponent<TextMesh>();
 	}
 
-    // 점수 더하기
-    public void addScore(int s)
+	void Update()
 	{
-        score += s;
-        scoreOutput.text = "점수 : " + score;
+		if (Input.GetButtonDown("Jump"))
+		{
+			if (transform.position.y < lowWarn)
+			{
+				GetComponent<Rigidbody>().velocity = new Vector3(0, jumpPower * jumpBoost, 0);
+			}
+			else
+			{
+				GetComponent<Rigidbody>().velocity = new Vector3(0, jumpPower, 0);
+			}
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	// 점수 더하기
+	public void addScore(int s)
+	{
+		score += s;
+		scoreOutput.text = "점수 : " + score;
 	}
 }
