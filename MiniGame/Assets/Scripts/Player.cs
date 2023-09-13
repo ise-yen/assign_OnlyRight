887e42bee15f59073dd8ef59355adf6b9c530e33
YeEun <ydkrkek@gmail.com>
2023-09-13 14:41:17 +0900

    [Add] 점수 시스템

--- MiniGame/Assets/Scripts/Player.cs
+++ MiniGame/Assets/Scripts/Player.cs
@@ -6,10 +6,13 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     public float jumpPower = 5;
+    TextMesh scoreOutput;
+    int score = 0;
 
     void Start()
     {
-        
+        // 이름으로 게임 오브젝트를 찾고, 그 중 TextMesh 컴포넌트를 얻기
+        scoreOutput = GameObject.Find(name: "Score").GetComponent<TextMesh>();
     }
 
 	void Update()
@@ -20,5 +23,12 @@ public class Player : MonoBehaviour
 	private void OnCollisionEnter(Collision collision)
 	{
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+    // 점수 더하기
+    public void addScore(int s)
+	{
+        score += s;
+        scoreOutput.text = "점수 : " + score;
 	}
 }
--- MiniGame/Assets/Scripts/Wall.cs
+++ MiniGame/Assets/Scripts/Wall.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class Wall : MonoBehaviour
 {
     public float speed = -5;
+    Player player;
+
     void Start()
     {
-        
+        player = GameObject.Find(name: "Player").GetComponent<Player>();
     }
 
     void Update()
     {
         transform.Translate(speed * Time.deltaTime, 0, 0);
-        if (transform.position.x < -10) Destroy(gameObject);
+        if (transform.position.x < -10)
+        {
+            Destroy(gameObject);
+            player.addScore(1);
+        }
     }
 }
